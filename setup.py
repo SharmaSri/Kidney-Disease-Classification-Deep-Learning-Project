9095142a4d097efdd57df9e69e92826ecc74b392
SharmaSri <sripriya1920@gmail.com>
2025-06-04 16:25:01 +0530

    I edited these files

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+
 __version__ = "0.0.0"
 
 REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
@@ -10,9 +11,9 @@ AUTHOR_USER_NAME = "SharmaSri"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "sripriya1920@gmail.com"
 
+
 setuptools.setup(
     name=SRC_REPO,
-    
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
@@ -24,5 +25,5 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(where="src")
 )
